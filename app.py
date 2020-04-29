99319887f9647e9bb6a9eb188d9a36a5dd4e6660
Reuben Wafula <rubewafula@gmail.com>
2020-04-29 13:00:23 +0300

    change logging on exception

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def after_request(response):
     # since that 500 is already logged via @app.errorhandler.
     if response.status_code != 500:
         ts = strftime('[%Y-%b-%d %H:%M]')
-        logger.error('%s %s %s %s %s %s',
+        app.logger.error('%s %s %s %s %s %s',
                       ts,
                       request.remote_addr,
                       request.method,
@@ -43,7 +43,7 @@ def exceptions(e):
     """ Logging after every Exception. """
     ts = strftime('[%Y-%b-%d %H:%M]')
     tb = traceback.format_exc()
-    logger.error('%s %s %s %s %s 5xx INTERNAL SERVER ERROR\n%s',
+    app.logger.error('%s %s %s %s %s 5xx INTERNAL SERVER ERROR\n%s',
                   ts,
                   request.remote_addr,
                   request.method,
